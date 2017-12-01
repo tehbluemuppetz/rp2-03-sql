bf387ce2880485611e12f3499c9d64fd58ea4e28
jonas.erik.vilhelm.werner@gmail.com <jonas.erik.vilhelm.werner@gmail.com>
2017-12-01 09:11:26 +0900

    additional excercises

--- 01_sql.py
+++ 01_sql.py
@@ -1,14 +1,14 @@
-# Create a SQLite3 database and table
+# INSERT Command
 # import the sqlite3 library
 import sqlite3
-# create a new database if the database doesn't already exist
+# create the connection object
 conn = sqlite3.connect("new.db")
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
-# create a table
-cursor.execute("""CREATE TABLE population
-                (city TEXT, state TEXT, population INT)
-                """)
-
+# insert data
+cursor.execute("INSERT INTO population VALUES('New York City','NY', 8400000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco','CA', 800000)")
+# commit the changes
+conn.commit()
 # close the database connection
 conn.close()
